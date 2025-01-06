6f580f4cb65e1aa56459fa337f892a39ea93730d
Jayden-Wilson-01 <jayden@tabber.co.uk>
2025-01-06 10:24:40 +0000

    Initial Project

---
+++ Jurassic-Park-Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Jurassic_Park_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
